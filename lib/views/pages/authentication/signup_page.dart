caa4fb6c194b6e53ef885a9e48e3ced09d572584
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2022-12-05 00:15:30 +0700

    Init authentication page

---
+++ lib/views/pages/authentication/signup_page.dart
@@ -0,0 +1,10 @@
+part of '_authentication.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
