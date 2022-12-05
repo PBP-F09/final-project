3114b7022148f643499b6a42a14fa22d28845aca
Fadhlan Hasyim <81791118+fadhlanhasyim@users.noreply.github.com>
2022-12-06 02:29:22 +0700

    1 authentication (#1)
    * Init authentication page
    
    * Add bottom navbar

--- lib/views/pages/homepage/_homepage.dart
+++ lib/views/pages/homepage/_homepage.dart
@@ -1,3 +1,8 @@
+import 'package:endterm_project/main.dart';
+import 'package:endterm_project/routes/routes_name.dart';
+import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
 import 'package:flutter/material.dart';
+
+import '../../widgets/globals/_globals.dart';
 
 part 'home_page.dart';
--- lib/views/pages/homepage/home_page.dart
+++ lib/views/pages/homepage/home_page.dart
@@ -1,10 +1,88 @@
 part of '_homepage.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const List<Widget> _pages = <Widget>[
+    MyHomePage(
+      title: 'test',
+    ),
+    MyHomePage(
+      title: 'title',
+    ),
+  ];
+
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: _pages.elementAt(_selectedIndex),
+      bottomNavigationBar: Container(
+        height: 65,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(
+              15,
+            ),
+            topLeft: Radius.circular(
+              15,
+            ),
+          ),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 3,
+              spreadRadius: 3,
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(
+              15,
+            ),
+            topRight: Radius.circular(
+              15,
+            ),
+          ),
+          child: FlashyTabBar(
+            selectedIndex: _selectedIndex,
+            showElevation: true,
+            onItemSelected: (index) => (setState(
+              () {
+                _selectedIndex = index;
+              },
+            )),
+            items: [
+              FlashyTabBarItem(
+                icon: const Icon(Icons.library_books_outlined),
+                title: const Text('Baca'),
+              ),
+              FlashyTabBarItem(
+                icon: const Icon(Icons.book),
+                title: const Text('Catat'),
+              ),
+              FlashyTabBarItem(
+                icon: const Icon(Icons.child_care),
+                title: const Text('Diary'),
+              ),
+              FlashyTabBarItem(
+                icon: const Icon(Icons.search),
+                title: const Text('Periksa'),
+              ),
+              FlashyTabBarItem(
+                icon: const Icon(Icons.question_answer_outlined),
+                title: const Text('Tanya'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
